1c43dd957e0ec537281a961a6bf6cef4dc3c0c1f
CamilaOmenna <camilaemidiomendes@gmail.com>
2022-10-10 13:06:15 -0300

    blogpessoal

---
+++ blogpessoal/src/postagem/entities/postagem.module.ts
@@ -0,0 +1,14 @@
+import { Controller, Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Postagem } from "./postagem.entity";
+
+@Module({
+imports: [TypeOrmModule.forFeature([Postagem])],
+providers:[],
+controllers:[],
+exports: [TypeOrmModule]
+
+})
+
+export class postagemModule{}
+
